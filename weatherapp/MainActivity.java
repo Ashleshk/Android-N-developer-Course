4469bff81eea4766fba3bc3f3edc1e136e456134
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-04-22 17:37:46 +0530

    Weather App
    Tells about weather status of Location as per the location entered by user and Toast friendlly comment whether to go out or its rainy out or  sunny out

---
+++ weatherapp/MainActivity.java
@@ -0,0 +1,164 @@
+package com.lenovo.weatherapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+//http://api.openweathermap.org/data/2.5/weather?q=london&APPID=bdb2e16603e00424937aed3aed6fb1a0
+
+    EditText cityName;
+    TextView resultTextView;
+
+    public void findWeather(View view) {
+
+        Log.i("cityName", cityName.getText().toString());
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(cityName.getWindowToken(), 0);
+
+        try {
+            String encodedCityName = URLEncoder.encode(cityName.getText().toString(), "UTF-8");
+
+            DownloadTask task = new DownloadTask();
+            task.execute("http://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName+"&APPID=bdb2e16603e00424937aed3aed6fb1a0");
+
+
+        } catch (UnsupportedEncodingException e) {
+
+            e.printStackTrace();
+
+            Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
+
+        }
+
+
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityName = (EditText) findViewById(R.id.cityName);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+
+                }
+
+                return result;
+
+            } catch (Exception e) {
+
+                Log.i("Could not find weather","");
+                resultTextView.setText(View.FIND_VIEWS_WITH_TEXT);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+
+                String message = "";
+
+                JSONObject jsonObject = new JSONObject(result);
+
+                String weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Weather content", weatherInfo);
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < arr.length(); i++) {
+
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    String main = "";
+                    String description = "";
+
+                    main = jsonPart.getString("main");
+                    description = jsonPart.getString("description");
+
+                    if (main != "" && description != "") {
+
+                        message += main + ": " + description + "\r\n";
+
+                    }
+
+                }
+
+                if (message != "") {
+
+                    resultTextView.setText(message);
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
+
+                }
+
+
+            } catch (JSONException e) {
+
+                Toast.makeText(getApplicationContext(), "Could not find weather", Toast.LENGTH_LONG);
+
+            }
+
+
+
+        }
+    }
+}
